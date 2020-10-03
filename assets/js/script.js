36f65ed5ee3730f872f16107d58181513717a29d
UpperCenter <tom@uppercenter.co>
2020-10-03 01:45:11 +0100

    test 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,33 @@ function date() {
 	document.getElementById("header_date").innerHTML = date;
 }
 
-function getCurrentTime() {
-	let ((this.getHours() < 10) ? "0" : "") + ((this.getHours() > 12) ? (this.getHours() - 12) : this.getHours()) + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds() + ((this.getHours() > 12) ? (' PM') : ' AM');
-};
+function displayClock() {
+	const monthNames = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+
+	var d = new Date();
+	var mm = monthNames[d.getMonth()];
+	var dd = d.getDate();
+
+	document.getElementById("time").innerText = d.toLocaleTimeString();
+
+	document.getElementById("month").innerText = mm;
+	document.getElementById("day").innerText = dd;
+
+	setTimeout(displayClock, 1000);
+}
 
 function greet() {
 	let currentTime = new Date();
